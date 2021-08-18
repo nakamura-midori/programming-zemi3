1494b3ee3bc6b72b543d1ce02309a89157c38be1
nakamura-midori <midonyanta714@gmail.com>
2021-08-18 13:51:24 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get  'signup' => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #get 'hello-world' => "foo#hello"
-
+  resources :users
 end
